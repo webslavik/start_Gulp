d7a110ce25d0090f4d4ca74f6235b9fef17db394
vyacheslav sykhanov <nogamegolife@gmail.com>
2016-08-06 07:22:17 +0400

    некоторые изменения

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,175 @@
+/*
+		----------------------------------------------------
+		Настройки Gulp файла
+		----------------------------------------------------
+*/
+
+
+/*
+	
+	Подключаемые плагины
+
+*/
+
 var gulp            = require('gulp'),
-    jade            = require('gulp-jade'),
-    sass            = require('gulp-sass'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    cleanCSS        = require('gulp-clean-css'),
-    concat          = require('gulp-concat'),
-    uglify          = require('gulp-uglify'),
-    notify          = require("gulp-notify"),
-    cache           = require('gulp-cache'),
-    rename          = require('gulp-rename'),
-    del             = require('del'),
-    browserSync     = require('browser-sync');
+		jade            = require('gulp-jade'),
+		sass            = require('gulp-sass'),
+		autoprefixer    = require('gulp-autoprefixer'),
+		cleanCSS        = require('gulp-clean-css'), // минификация css
+		concat          = require('gulp-concat'), // объединение js
+		uglify          = require('gulp-uglify'), // минифицируем js
+		notify          = require("gulp-notify"), // выводим ошибки
+		cache           = require('gulp-cache'),
+		rename          = require('gulp-rename'),
+		sftp            = require('gulp-sftp'), // отправка файлов на сервер
+		del             = require('del'),
+		browserSync     = require('browser-sync');
 
 
+
+
+
+/*
+	
+	Наши задачи
+
+*/
+
+
+
+// Jade
+//--------------------------------------------------
 gulp.task('jade', function() {
-    return gulp.src('src/jade/index.jade')
-    .pipe(jade({
-        pretty: true
-    }))
-    .on('error', notify.onError(function(err) {
-        return {
-          title: 'Sass',
-          message: err.message
-        }
-    }))
-    .pipe(gulp.dest('src/'));
+	return gulp.src('app/jade/*.jade')
+	.pipe(jade({
+			pretty: true
+	}))
+	.on('error', notify.onError(function(err) {
+			return {
+				title: 'Jade',
+				message: err.message
+			}
+	}))
+	.pipe(gulp.dest('app/'));
 });
 
+
+// Sass
+//--------------------------------------------------
 gulp.task('sass', function() {
-    return gulp.src('src/sass/main.sass')
-        .pipe(sass())
-        .on('error', notify.onError(function(err) {
-            return {
-              title: 'Sass',
-              message: err.message
-            }
-        }))
-        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('src/css/'))
-        .pipe(cleanCSS())
-        // .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('src/css/'))
-        .pipe(browserSync.reload({stream: true}));
+	return gulp.src('app/sass/*.sass')
+		.pipe(sass())
+		.on('error', notify.onError(function(err) {
+			return {
+				title: 'Sass',
+				message: err.message
+			}
+		}))
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(gulp.dest('app/css/'))
+		// .pipe(cleanCSS())
+		// .pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('app/css/'))
+		.pipe(browserSync.reload({stream: true}));
 })
 
-// gulp.task('css-min', ['sass'], function() {
-//   return gulp.src('src/css/main.css')
-//     .pipe(cleanCSS())
-//     .pipe(rename({suffix: '.min'}))
-//     .pipe(gulp.dest('src/css'));
-// })
+
+// Для объединения стороних библиотек
+//--------------------------------------------------
+gulp.task('css-vendor', function() {
+	return gulp.src([
+			'app/bower_components/normalize-css/normalize.css',
+			'app/bower_components/bootstrap/dist/css/bootstrap.css'
+		])
+		.pipe(concat('vendor.css')) // объединяем стороние бибилиотеки
+		.pipe(cleanCSS())
+		.pipe(rename({basename: 'vendor'}))
+		.pipe(gulp.dest('app/css'))
+})
 
 
+// Скрипты
+//--------------------------------------------------
 gulp.task('scripts', function() {
-    return gulp.src([
-        'src/js/jquery-3.0.0.min.js',
-        'src/js/function.js'
-    ])
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('src/js'));
-})
-
-
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: 'src/'
-        },
-        notify: false
-    });
+		return gulp.src([
+			'app/bower_components/jquery/dist/jquery.js'
+			// 'app/js/function.js'
+			])
+			.pipe(concat('vendor.js')) // объединяем стороние бибилиотеки
+			.pipe(uglify())
+			.pipe(gulp.dest('app/js'))
 });
 
 
-gulp.task('watch', ['browser-sync',  'sass', 'jade', 'scripts'], function() {
-    gulp.watch('src/sass/**/*', ['sass']);
-    gulp.watch('src/jade/**/*.jade', ['jade']);
-    gulp.watch('src/*.html', browserSync.reload);
-    gulp.watch('src/js/**/*', browserSync.reload);
-})
+// Сервер
+//--------------------------------------------------
+gulp.task('browser-sync', function() {
+		browserSync({
+				server: {
+						baseDir: 'app/'
+				},
+				notify: false
+		});
+});
 
 
+// Отправляем файлы на сервер 
+//--------------------------------------------------
+gulp.task('sftp', function () {
+		return gulp.src('dist/**/*')
+				.pipe(sftp({
+						host: 'website.com',
+						user: 'johndoe',
+						pass: '1234'
+				}));
+});
 
-// Images
+
+// Удаляем папку dist
 //--------------------------------------------------
 gulp.task('clean', function() {
-    return del.sync('dist');
+		return del.sync('dist');
 });
 
+
+// Для изображений
+//--------------------------------------------------
 gulp.task('clear', function() {
-    return cache.clearAll();
+		return cache.clearAll();
 });
 
 
 
-// Build project
+// Собираем проект
 //--------------------------------------------------
 gulp.task('build', ['clean', 'sass', 'scripts', 'jade'], function() {
 
-    var buildCss = gulp.src('src/css/*')
-      .pipe(gulp.dest('dist/css/'))
+		var buildCss = gulp.src('app/css/*')
+			.pipe(gulp.dest('dist/css/'))
 
-    var buildFonts = gulp.src('src/fonts/**/*')
-      .pipe(gulp.dest('dist/fonts/'));
+		var buildFonts = gulp.src('app/fonts/**/*')
+			.pipe(gulp.dest('dist/fonts/'));
 
-    var buildJS = gulp.src('src/js/**/*')
-      .pipe(gulp.dest('dist/js/'));
+		var buildJS = gulp.src('app/js/**/*')
+			.pipe(gulp.dest('dist/js/'));
 
-    var buildHTML = gulp.src('src/*.html')
-      .pipe(gulp.dest('dist/'));
+		var buildHTML = gulp.src('app/*.html')
+			.pipe(gulp.dest('dist/'));
 
-    var buildImg = gulp.src('src/img/**/*')
-      .pipe(gulp.dest('dist/img/'));
+		var buildImg = gulp.src('app/img/**/*')
+			.pipe(gulp.dest('dist/img/'));
 });
+
+
+
+// Наблюдаем за нашими файлами
+//--------------------------------------------------
+gulp.task('watch', ['browser-sync',  'sass', 'jade', 'scripts'], function() {
+		gulp.watch('app/sass/**/*', ['sass']);
+		gulp.watch('app/jade/**/*.jade', ['jade']);
+		gulp.watch('app/*.html', browserSync.reload);
+		gulp.watch('app/js/**/*', browserSync.reload);
+})
 
 
 // Default
